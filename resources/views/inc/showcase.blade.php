1d73d56be96e01bfa1dcf2488237b842e602fe6f
prakash18 <prkjha25@gmail.com>
2018-09-26 12:06:36 +0530

    this is basic app using laravel

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,7 @@
+<div class="jumbotron text-center">
+	<div class="container">
+		<h1>welcome to our site</h1>
+		<p class="lead"> welcome to our brand new laravel. This is the larevel version 5.4</p>
+	</div>
+</div>	
+		
